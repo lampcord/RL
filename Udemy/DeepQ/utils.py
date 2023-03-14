e8c3bcc2caaa99aac5e0041986c847e4d3327c02
lampcord <33327028+lampcord@users.noreply.github.com>
2023-03-13 21:49:58 -0400

    Added more utility classes and functions

--- Udemy/DeepQ/utils.py
+++ Udemy/DeepQ/utils.py
@@ -71,4 +71,35 @@ class PreprocessFrame(gym.ObservationWrapper):
 
         return new_obs
 
+class StackFrames(gym.ObservationWrapper):
+    def __init__(self, env, repeat):
+        super(StackFrames, self).__init__(env)
+        self.observation_space = gym.spaces.Box(
+            env.observation_space.low.repeat(repeat, axis=0),
+            env.observation_space.high.repeat(repeat, axis=0),
+            dtype=np.float32)
+        self.stack = collections.deq(maxlen=repeat)
+
+    def reset(self):
+        self.stack.clear()
+        observation = self.env.reset()
+        for _ in range(self.stack.maxlen):
+            self.stack.append(observation)
+
+        return np.array(self.stack).reshape(self.observation_space.low.shape)
+
+    def observation(self, observation):
+        self.stack.append(observation)
+
+        return np.array(self.stack).reshape(self.observation_space.low.shape)
+
+
+def make_env(env_name, shape=(84,84,1), repeat=4):
+    env = gym.make(env_name)
+    env = RepeatActionsAndMaxFrame(env, repeat)
+    env = PreprocessFrame(shape, env)
+    env = StackFrames(env, repeat)
+
+    return env
+
 

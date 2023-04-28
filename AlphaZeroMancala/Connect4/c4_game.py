2b58cdd1a7ffb59b99e7b8e65501ca39eecd71f9
lampcord <33327028+lampcord@users.noreply.github.com>
2023-04-28 19:56:50 -0400

    Added function to double check move's win detection and provide path of winning move.

--- AlphaZeroMancala/Connect4/c4_game.py
+++ AlphaZeroMancala/Connect4/c4_game.py
@@ -49,7 +49,7 @@ All the code is generic, so, in theory you can try to adjust the field size.
 But tests could become broken.
 """
 import random
-
+import numpy as np
 import game
 
 GAME_ROWS = 6
@@ -59,7 +59,12 @@ PLAYER_BLACK = 1
 PLAYER_WHITE = 0
 COUNT_TO_WIN = 4
 
-
+winning_sets = [
+    (0, 1),
+    (1, 1),
+    (1, 0),
+    (1, -1)
+]
 class C4Game(game.Game):
     def __init__(self):
         super().__init__()
@@ -68,7 +73,29 @@ class C4Game(game.Game):
         return self.get_encoded_binary([[]] * GAME_COLS, game.GameTurn.PLAYER1)
 
     def move(self, binary_state, move, turn):
-        raise NotImplementedError("move")
+        assert isinstance(binary_state, int)
+        assert isinstance(move, int)
+        assert 0 <= move < GAME_COLS
+        assert turn == game.GameTurn.PLAYER1 or turn == game.GameTurn.PLAYER2
+        list_state = self.get_decoded_list(binary_state, turn)
+        assert len(list_state[move]) < GAME_ROWS
+        list_state[move].append(turn.value)
+        # check for victory: the simplest vertical case
+        suff = list_state[move][-COUNT_TO_WIN:]
+        won = suff == [turn.value] * COUNT_TO_WIN
+        if not won:
+            won = self._check_won(list_state, move, 0) or self._check_won(list_state, move, 1) or self._check_won(list_state, move, -1)
+
+        binary_state = self.get_encoded_binary(list_state, turn)
+
+        result = game.GameResult.NOT_COMPLETED
+        if won:
+            result = game.GameResult.PLAYER1 if turn == game.GameTurn.PLAYER1 else game.GameResult.PLAYER2
+        else:
+            if len(self.get_legal_moves(binary_state, turn)) == 0:
+                result = game.GameResult.DRAW
+
+        return binary_state, result, True, {}
 
     def get_legal_moves(self, binary_state, turn):
         assert isinstance(binary_state, int)
@@ -109,6 +136,34 @@ class C4Game(game.Game):
 
         return list_state
 
+    def _check_won(self, field, col, delta_row):
+        player = field[col][-1]
+        coord = len(field[col]) - 1
+        total = 1
+        # negative dir
+        cur_coord = coord - delta_row
+        for c in range(col - 1, -1, -1):
+            if len(field[c]) <= cur_coord or cur_coord < 0 or cur_coord >= GAME_ROWS:
+                break
+            if field[c][cur_coord] != player:
+                break
+            total += 1
+            if total == COUNT_TO_WIN:
+                return True
+            cur_coord -= delta_row
+        # positive dir
+        cur_coord = coord + delta_row
+        for c in range(col + 1, GAME_COLS):
+            if len(field[c]) <= cur_coord or cur_coord < 0 or cur_coord >= GAME_ROWS:
+                break
+            if field[c][cur_coord] != player:
+                break
+            total += 1
+            if total == COUNT_TO_WIN:
+                return True
+            cur_coord += delta_row
+        return False
+
     def bits_to_int(self, bits):
         res = 0
         for b in bits:
@@ -130,32 +185,95 @@ class C4Game(game.Game):
         return list_state
 
 
-    def render(self, binary_state, turn):
+    def render(self, binary_state, turn, win=None):
         list_state = self.get_decoded_list(binary_state, turn)
-        data = [[' '] * GAME_COLS for _ in range(GAME_ROWS)]
+        data = [['   '] * GAME_COLS for _ in range(GAME_ROWS)]
         for col_idx, col in enumerate(list_state):
             for rev_row_idx, cell in enumerate(col):
                 row_idx = GAME_ROWS - rev_row_idx - 1
-                data[row_idx][col_idx] = str(cell)
-        display_state = [''.join(row) for row in data]
+                if win and (col_idx, row_idx) in win:
+                    data[row_idx][col_idx] = f"({str(cell)})"
+                else:
+                    data[row_idx][col_idx] = f" {str(cell)} "
+        display_state = ['|'.join(row) for row in data]
         for line in display_state:
-            print(line)
+            print ('+---+---+---+---+---+---+---+')
+            print('|' + line + '|')
+        print ('+---+---+---+---+---+---+---+')
 
-if __name__ == "__main__":
+    def check_for_win(self, binary_state, turn):
+        table = np.full((GAME_COLS, GAME_ROWS), -1)
+        list_state = self.get_decoded_list(binary_state, turn)
+        data = [[-1] * GAME_COLS for _ in range(GAME_ROWS)]
+        for col_idx, col in enumerate(list_state):
+            for rev_row_idx, cell in enumerate(col):
+                row_idx = GAME_ROWS - rev_row_idx - 1
+                table[col_idx][row_idx] = str(cell)
+        for row in range(GAME_ROWS):
+            for col in range(GAME_COLS):
+                for winning_set in winning_sets:
+                    start_x = col
+                    start_y = row
+                    win = [(start_x, start_y)]
+                    start_symbol = table[start_x][start_y]
+                    if start_symbol < 0:
+                        continue
+
+                    for pos in range(COUNT_TO_WIN - 1):
+                        start_x += winning_set[0]
+                        start_y += winning_set[1]
+                        if start_x >= 0 and start_x < GAME_COLS and start_y >= 0 and start_y < GAME_ROWS:
+                            if table[start_x][start_y] == start_symbol:
+                                win.append((start_x, start_y))
+                            else:
+                                break
+                    if len(win) == 4:
+                        return win
+        return None
+
+
+def play_random_game(show=False):
     c4_game = C4Game()
     turn = game.GameTurn.PLAYER1
     binary_state = c4_game.get_initial_position()
 
-    while True:
-        c4_game.render(binary_state, turn)
-        print()
+    result = game.GameResult.NOT_COMPLETED
+    win_set = None
+    while result == game.GameResult.NOT_COMPLETED:
+        if show:
+            print(binary_state)
+            c4_game.render(binary_state, turn, win_set)
+            print()
         legal_moves = c4_game.get_legal_moves(binary_state, turn)
         if len(legal_moves) == 0:
             break
         move = random.choice(legal_moves)
-        list_state = c4_game.get_decoded_list(binary_state, turn)
-        list_state[move].append(1 if turn == game.GameTurn.PLAYER1 else 0)
-        turn = c4_game.get_oponents_turn(turn)
-        binary_state = c4_game.get_encoded_binary(list_state, turn)
-    c4_game.render(binary_state, turn)
+        binary_state, result, swap_players, info = c4_game.move(binary_state, move, turn)
+        win_set = c4_game.check_for_win(binary_state, turn)
+
+        if win_set:
+            if result == game.GameResult.NOT_COMPLETED or result == game.GameResult.DRAW:
+                print(f"win_set {str(win_set)} result {result}")
+                print(binary_state)
+                c4_game.render(binary_state, turn, win_set)
+        else:
+            if result == game.GameResult.PLAYER1 or result == game.GameResult.PLAYER2:
+                print(f"win_set {str(win_set)} result {result}")
+                print(binary_state)
+                c4_game.render(binary_state, turn, win_set)
+
+        if swap_players:
+            binary_state, turn = c4_game.switch_players(binary_state, turn)
+    if show:
+        print(binary_state)
+        c4_game.render(binary_state, turn, win_set)
+
+if __name__ == "__main__":
+    max = 10000
+    for ndx in range(max):
+        if ndx % (max / 100) == 0:
+            print()
+        print('.', end='')
+        play_random_game()
+
 

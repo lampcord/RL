ddd8797c98283effa4689b0a5abee309a78dbfd2
lampcord <33327028+lampcord@users.noreply.github.com>
2023-05-18 18:18:20 -0400

    Update main.py

--- AlphaZeroMancala/main.py
+++ AlphaZeroMancala/main.py
@@ -21,9 +21,6 @@ game_rules = C4GameRules()
 console_renderer = C4ConsoleRenderer()
 gui_renderer = C4GuiRenderer()
 
-console_renderer.render(279174667118, 0)
-exit()
-
 # Load the DLL
 dll_path = os.path.join("./connect_4/FastRollout.dll")
 my_functions = ctypes.CDLL(dll_path)
@@ -39,7 +36,7 @@ restart_function = my_functions.C4_start_new_game
 
 my_functions.C4_set_parameters.argtypes = [ctypes.c_char_p, ctypes.c_uint32, ctypes.c_uint32]
 my_functions.C4_set_parameters.restype = None
-my_functions.C4_set_parameters(b".\\connect_4\\test.bin", 100000, 100000)
+my_functions.C4_set_parameters(b".\\connect_4\\test.bin", 2000000, 1000000)
 
 my_functions.C4_save.argtypes = []
 my_functions.C4_save.restype = None
@@ -70,7 +67,7 @@ mcts_config_rollout_vs_A0 = MCTSAgentConfig()
 mcts_config_rollout_vs_A0.loops = 1000000
 mcts_config_rollout_vs_A0.rollout_policy = rollout_policy
 mcts_config_rollout_vs_A0.rollout_count = 1
-mcts_config_rollout_vs_A0.max_time = 0.1
+mcts_config_rollout_vs_A0.max_time = 1.0
 mcts_config_rollout_vs_A0.restart_function = restart_function
 
 play_against_random_console = [game_rules, [UserAgent(game_rules, console_renderer), RandomAgent(game_rules)], 2, console_renderer]
@@ -81,8 +78,8 @@ play_random = [game_rules, [RandomAgent(game_rules), RandomAgent(game_rules)], 1
 play_random_mcts = [game_rules, [RandomAgent(game_rules), MCTSAgent(game_rules, mcts_config_default)], 1000, None]
 play_mcts_mcts = [game_rules, [MCTSAgent(game_rules, mcts_config_rollout_1000_1sec), MCTSAgent(game_rules, mcts_config_rollout_1_1sec)], 1000, None]
 play_console_console = [game_rules, [UserAgent(game_rules, console_renderer), UserAgent(game_rules, console_renderer)], 10, console_renderer]
-# train_mcts_mcts = [game_rules, [MCTSAgent(game_rules, mcts_config_rollout_vs_A0), MCTSAgent(game_rules, mcts_config_rollout_vs_A0)], 320, None]
-train_mcts_mcts = [game_rules, [MCTSAgent(game_rules, mcts_config_rollout_vs_A0), MCTSAgent(game_rules, mcts_config_rollout_vs_A0)], 10, None]
+train_mcts_mcts = [game_rules, [MCTSAgent(game_rules, mcts_config_rollout_vs_A0), MCTSAgent(game_rules, mcts_config_rollout_vs_A0)], 320, None]
+# train_mcts_mcts = [game_rules, [MCTSAgent(game_rules, mcts_config_rollout_vs_A0), MCTSAgent(game_rules, mcts_config_rollout_vs_A0)], 10, None]
 
 director = TournamentDirector(*train_mcts_mcts)
 tournament_set = director.run()

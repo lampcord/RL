328ce6eceaaf686aeb800cee178debc693795a04
lampcord <33327028+lampcord@users.noreply.github.com>
2023-05-19 17:16:57 -0400

    Update dump_key.py

--- AlphaZeroMancala/dump_key.py
+++ AlphaZeroMancala/dump_key.py
@@ -1,7 +1,17 @@
 from connect_4.c4_console_renderer import C4ConsoleRenderer
+import argparse
 
-keys = (35734131496540, 35734131496541)
-for key in keys:
+parser = argparse.ArgumentParser(description="Enter positions separated by spaces")
+parser.add_argument('args', metavar='N', type=str, nargs='*', help="Variable length arguments")
+
+args = parser.parse_args()
+
+for arg in args.args:
+    print(arg)
+
+keys = (args.args)
+for skey in keys:
+    key = int(skey)
     turn = key & 0x1
     position = key // 2
     renderer = C4ConsoleRenderer()

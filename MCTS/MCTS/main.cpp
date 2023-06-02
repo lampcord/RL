c0eabcd8bd86fd0c8d5c615565e97cd63a4df6fa
lampcord <33327028+lampcord@users.noreply.github.com>
2023-06-01 22:06:09 -0400

    get_nth_move

--- MCTS/MCTS/main.cpp
+++ MCTS/MCTS/main.cpp
@@ -22,6 +22,11 @@ int main()
 	TestGame::get_initial_position(position);
 	TestGame::get_legal_moves(position, turn, legal_moves);
 	auto num_moves = get_num_moves<unsigned char>(legal_moves);
+	for (auto x = 0u; x < num_moves; x++)
+	{
+		auto move = get_nth_move(legal_moves, x);
+		cout << (unsigned int)move << endl;
+	}
 
 	mcts.find_move(position, turn);
 }

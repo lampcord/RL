12b38b59497638828e06b4091e923c574fd501e2
lampcord <33327028+lampcord@users.noreply.github.com>
2023-07-18 20:49:29 -0400

    Update Gammon.cpp

--- Gammon/Gammon.cpp
+++ Gammon/Gammon.cpp
@@ -6,12 +6,14 @@ using namespace BackgammonNS;
 int main()
 {
 	MoveList move_list;
+	Backgammon::run_position_tests("C:\\GitHub\\RL\\test_games.txt", false);
+	return 0;
+
 	PositionStruct position;
 	Backgammon::get_initial_position(position);
 	Backgammon::position_from_string("@02  0  0  0  0B05  0B04  0  0  0W03B04  0  0  0W02W01W03B01  0W02  0W02  0  1", position);
 	Backgammon::render(position);
 	Analyzer::analyze(position);
 
-	//Backgammon::run_position_tests("C:\\GitHub\\RL\\test_games.txt", false);
 }
 

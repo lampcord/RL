9b341705a113d58105d165d5e6ced72ce7b03f04
lampcord <33327028+lampcord@users.noreply.github.com>
2024-02-26 19:33:28 -0500

    Checkin

--- bgedit2/show_history.py
+++ bgedit2/show_history.py
@@ -15,9 +15,11 @@ import bg_board
 from XGID_to_position import *
 
 WINDOW_SIZE = (800, 800)
+EXTENDED_WINDOW_SIZE = (1500, 800)
 BG_COLOR = (192, 192, 192)
 COLUMNS = 3
 COLUMN_WIDTH = WINDOW_SIZE[0] // (COLUMNS + 1)
+CAT_COLS = (800, 1000)
 
 bg_board.update_theme('greyscale.json')
 
@@ -42,28 +44,62 @@ except Exception as e:
     print(f'ERROR: Problem loading quiz {str(e)}')
     sys.exit(1)
 
-categories = [
-    'Opening',
-    'Middlegame',
-    'EG - Ace-point Holding',
-    'EG - Bearing-In',
-    'EG - Bearing-Off',
-    'DBL - Passes',
-    'DBL - Post-Crawford',
-    'DBL - Takes',
-    'DBL - Too Good',
-    'Plan - Backgame',
-    'Plan - Blitz',
-    'Plan - Holding Game',
-    'Plan - Prime',
-    'Plan - Race'
-]
 
-category_map = {}
+category_map = {
+    '0': ['Opening', 'First 4 moves'],
+    '1': ['Middlegame', 'Both players have men back and developing.'],
+    '2': ['Blitz - O', 'Attacking and building points. Defender has blots or on bar.'],
+    '3': ['Blitz - D', 'Attacking and building points. Defender has blots or on bar.'],
+    '4': ['Long Race', 'Bearing in no contact.'],
+    '5': ['Bearoff Race', 'Bearing off no contact.'],
+    '6': ['Bearoff W/Cont - O', 'Bearing off with oponents back.'],
+    '7': ['Bearoff W/Cont - D', 'Bearing off with oponents back.'],
+    '8': ['Roll vs Roll', 'Late bearoff where you are counting rolls not pips.'],
+    '9': ['Mutual Holding', 'Both players have high anchor and are trying to hold and run'],
+    'A': ['One Way Holding - O', 'High anchor (6, 7, 9) vs. no one back.'],
+    'B': ['One Way Holding - D', 'High anchor (6, 7, 9) vs. no one back.'],
+    'C': ['Deep Anchor - O', 'Low anchor (1, 2) vs. no one back.'],
+    'D': ['Deep Anchor - D', 'Low anchor (1, 2) vs. no one back.'],
+    'E': ['3 Pt. Anchor - O', '3 point anchor vs. no one back.'],
+    'F': ['3 Pt. Anchor - D', '3 point anchor vs. no one back.'],
+    'G': ['No Anchor H - O', 'Holding game with no anchor but multiple blots.'],
+    'H': ['No Anchor H - D', 'Holding game with no anchor but multiple blots.'],
+    'I': ['One Man Back - O', 'Holding game with no anchor but one blot.'],
+    'J': ['One Man Back - D', 'Holding game with no anchor but one blot.'],
+    'K': ['6 Prime - O', 'One side has a 6 prime.'],
+    'L': ['6 Prime - D', 'One side has a 6 prime.'],
+    'M': ['Late Game Cont - O', 'Holding game at end.'],
+    'N': ['Late Game Cont - D', 'Holding game at end.'],
+    'O': ['Early Bk Game - O', 'Back game where both sides have men back'],
+    'P': ['Early Bk Game - D', 'Back game where both sides have men back'],
+    'Q': ['Mid Bk Game - O', 'Back game where offense has escaped.'],
+    'R': ['Mid Bk Game - D', 'Back game where offense has escaped.'],
+    'S': ['Late Bk Game - O', 'Bearing off / in'],
+    'T': ['Late Bk Game - D', 'Bearing off / in'],
+    'U': ['Containment - O', 'Post hit contain'],
+    'V': ['Containment - D', 'Post hit contain'],
+    'W': ['Stack Straggle - O', 'Bear off vs Straggler'],
+    'X': ['Stack Straggle - D', 'Bear off vs Straggler'],
+    'Y': ['Prime v Prime', 'Both priming with trapped checkers.'],
+    'Z': ['Blitz v Prime - B', 'One side blitzing other sid priming.'],
+    ',': ['Blitz v Prime - P', 'One side blitzing other sid priming.'],
+    '.': ['Crunch', 'Both sides have men back trying not to crunch'],
+    '/': ['Hypergammon', 'Both players trying to get 3 or less around and past each other.']
+}
+
 key = 'A'
-for category in categories:
-    category_map[key] = category
-    key = chr(ord(key) + 1)
+key_mappings = {}
+for k in range(pygame.K_a, pygame.K_z + 1):
+    key = chr(ord('A') + k - pygame.K_a)
+    key_mappings[k] = key
+
+for k in range(pygame.K_0, pygame.K_9 + 1):
+    key = chr(ord('0') + k - pygame.K_0)
+    key_mappings[k] = key
+
+key_mappings[pygame.K_COMMA] = ','
+key_mappings[pygame.K_PERIOD] = '.'
+key_mappings[pygame.K_SLASH] = '/'
 
 print(category_map)
 decay = 0.90
@@ -73,7 +109,7 @@ qc = QuizController(directory + 'qc.json', len(quiz), decay, new_mult)
 
 pygame.init()
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode(WINDOW_SIZE)
+screen = pygame.display.set_mode(EXTENDED_WINDOW_SIZE)
 pygame.display.set_caption("Backgammon History")
 font = pygame.font.Font(None, 24)
 
@@ -105,11 +141,6 @@ def display_text(x, y, text, color, target, center_x=True, center_y=True):
 def get_text_size(text):
     text_surface = font.render(text, True, (0, 0, 0,))  # Text, antialiasing, color
     return text_surface.get_size()
-
-max_category_width = 0
-for cat in categories:
-    w, h = get_text_size(cat)
-    max_category_width = max(w, max_category_width)
 
 key = ''
 
@@ -145,22 +176,22 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 pyperclip.copy("XGID=" + quiz_line['flashcard'])
-            if pygame.K_a <= event.key <= pygame.K_z:
-                key = chr(ord('A') + event.key - pygame.K_a)
-                print(key)
-                if key == 'Q':
+            if event.key == pygame.K_ESCAPE:
+                running = False
+                break
+            elif event.key == pygame.K_TAB:
+                current_key += 1
+                if current_key >= len(hist_keys):
                     running = False
                     break
-                elif key == 'X':
-                    current_key += 1
-                    if current_key >= len(hist_keys):
-                        running = False
-                        break
-                    quiz_line_index = int(hist_keys[current_key])
-                    quiz_line = quiz[quiz_line_index]
-                    image = load_flashcard(quiz_line)
-                    current_categories = qc.categories.get(str(quiz_line_index), [])
-                elif key in category_map.keys():
+                quiz_line_index = int(hist_keys[current_key])
+                quiz_line = quiz[quiz_line_index]
+                image = load_flashcard(quiz_line)
+                current_categories = qc.categories.get(str(quiz_line_index), [])
+            elif event.key in key_mappings.keys():
+                key = key_mappings[event.key]
+                print(key)
+                if key in category_map.keys():
                     if key in current_categories:
                         current_categories.remove(key)
                     else:
@@ -183,6 +214,7 @@ while running:
     for c, s in hist:
         counts[c] = counts.get(c, 0) + 1
 
+    text_line += line_spacing
     for x in range(len(quiz_line['answers'])):
         color = (0, 0, 0)
         c = counts.get(x, 0)
@@ -197,22 +229,21 @@ while running:
         text_line += line_spacing
 
     text_line += line_spacing
+    display_text(center, text_line, 'Press X to continue or Q to quit', (0, 0, 0), screen)
 
-    col = 0
+    text_line += line_spacing
+
+    text_line = line_spacing
     for k in category_map.keys():
         color = (0, 0, 0)
         if k in current_categories:
             color = (0, 128, 0)
 
-        display_text(columns[col] - max_category_width // 2, text_line, f'{k}) {category_map[k]}', color, screen, center_x=False)
-        col += 1
-        if col >= COLUMNS:
-            col = 0
-            text_line += line_spacing
+        display_text(CAT_COLS[0], text_line, f'{k}) {category_map[k][0]}', color, screen, center_x=False)
+        display_text(CAT_COLS[1], text_line, f'{category_map[k][1]}', color, screen, center_x=False)
+        text_line += line_spacing
 
 
-    text_line += line_spacing
-    display_text(center, text_line, 'Press X to continue or Q to quit', (0, 0, 0), screen)
 
     pygame.display.flip()
 
